b7c6796faf693fed5d71a3cfb36c9be9455d2703
ronia <ronialon2008@gmail.com>
2022-03-26 13:13:37 +0300

    comments

--- Q2/src/Main.java
+++ Q2/src/Main.java
@@ -1,59 +1,73 @@
+/*
+Author: Roni Alon 315565176
+The class gets 2 rational numbers inputs from the user and use the Rational class methods
+ */
+
 import java.util.InputMismatchException;
-        import java.util.Scanner;
+import java.util.Scanner;
+
+import static java.lang.Integer.parseInt;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int p, q;
-        Rational num = null;
-        Rational operations= null;
-        Rational reduce= null;
+        Rational operations,reduce;
         boolean flag=true;
-        while (flag){
-            System.out.println("please enter two numbers numerator and a denominator");
-//            scan.nextLine(); //empty scanner object
-            try {
-                p = scan.nextInt();
-                q = scan.nextInt();
-                num = new Rational(p, q);
+        Rational firstNum = getRational(scan, flag);
+        System.out.println("The first Rational number you entered is " + firstNum.toString());
+        System.out.println("Pleases enter your second Rational number");
+        Rational secondNum= getRational(scan,flag);
+        System.out.println("The second valid Rational number you entered is " + secondNum.toString());
 
-            } catch (IllegalArgumentException e) {
-                System.out.println("please enter a valid number");
-                continue;
-            } catch (InputMismatchException err) {
-                System.out.println("please enter a valid numbers - not an integer");
-                continue;
-            }
-            flag=false; //we got a valid input
-        }
-        Rational other= new Rational(1,3);
-        boolean result = num.greaterThan(other);
-        System.out.println("does num is grater than other?" + result);
-        result = num.equals(other);
-        System.out.println("does num is equals to other?" + result);
-        operations= num.plus(other);
-        System.out.println("The sum of num and other is " + operations.toString());
-        reduce=operations.reduce();
-        System.out.println("The reduced sum of num and other is " + reduce.toString());
-        operations=num.minus(other);
-        System.out.println("The minus of num and other is " + operations.toString());
-        reduce=operations.reduce();
-        System.out.println("The reduced minus of num and other is " + reduce.toString());
-        operations=num.multiply(other);
-        System.out.println("The multiply of num and other is " + operations.toString());
-        reduce=operations.reduce();
-        System.out.println("The reduced multiply of num and other is " + reduce.toString());
+        //check if first number is grater than the second number - return bool value
+        System.out.println("does num is grater than other?" + firstNum.greaterThan(secondNum));
+
+        //check if first number is equals to the second number - return bool value
+        System.out.println("does num is equals to other?" + firstNum.equals(secondNum));
+
+        //print the sum of both rationals
+        System.out.println("The sum of first rational and second rational is " + firstNum.plus(secondNum).reduce().toString());
+
+        //print the minus of both rationals
+        System.out.println("The reduced minus of num and other is " + firstNum.minus(secondNum).reduce().toString());
+
+        //print the multiply value of both rationals
+        System.out.println("The multiply of num and other is " + firstNum.multiply(secondNum).reduce().toString());
+
         try{
-            operations=num.divide(other);
-            System.out.println("The divide of num and other is " + operations.toString());
-            reduce=operations.reduce();
-            System.out.println("The reduced divide of num and other is " + reduce.toString());
+            //print the divide value of both rationals
+            System.out.println("The reduced divide of num and other is " + firstNum.divide(secondNum).reduce().toString());
         } catch (ArithmeticException e) {
             System.out.println("Impossible to divide in 0");
         }
 
+    }
 
+    //This method gets two numbers and create a new Rational number object from them
+    private static Rational getRational(Scanner scan, boolean flag) {
+        int p;
+        int q;
+        Rational num=null;
 
+        while (flag) {
+            System.out.println("please enter two numbers numerator and a denominator");
+            try {
+                p = scan.nextInt();
+                q = scan.nextInt();
+                num = new Rational(p, q);
+            } catch (IllegalArgumentException err) { //error from the constructor
+                System.out.println("Please enter a valid numbers - the numerator is an Integer , the denominator must be positive");
+                scan.nextLine(); //empty scanner object
+                continue;
+           }
+            catch (InputMismatchException err) { //error of illegal input - only integers
+                System.out.println("please enter a valid number only Integers");
+                scan.nextLine(); //empty scanner object
+                continue;
+            }
+            flag = false; //we got a valid input
+        }
+        return num;
     }
 }
 
--- Q2/src/Rational.java
+++ Q2/src/Rational.java
@@ -1,3 +1,9 @@
+/*
+Author: Roni Alon 315565176
+The class create a rational number object by a given numerator and denominator,
+and have the functions you can do on two rational numbers - grater than,equals, sum, minus, multiply , divide
+ */
+
 import static java.lang.Math.*;
 
 public class Rational {
@@ -25,41 +31,45 @@ public class Rational {
         return this.p + "/" + this.q;
     }
 
+    //This method checks if this rational is grater than the param rational - return bool value
     public boolean greaterThan(Rational other) {
         return this.p * other.q > this.q * other.p;
     }
 
+    //This method checks if this rational is equals than the param rational - return bool value
     public boolean equals(Rational other) {
         return this.p * other.q == this.q * other.p;
     }
-
+    //This method returns a Rational obj that represents the sum of this rational number and param rational
     public Rational plus(Rational other) {// a/b+c/d= (ad+bc)/bd
         int newNumerator = this.p * other.q + this.q * other.p;
         int newDenominator = this.q * other.q;
         return new Rational(newNumerator, newDenominator);
     }
 
+    //This method returns a Rational obj that represents the minus of this rational number and param rational
     public Rational minus(Rational other) {// a/b-c/d= (ad-bc)/bd => (ad+bc-2bc)/bd so its the same denominator but -2bc/bd from the plus
         Rational plus = this.plus(other);
         int newNumerator = plus.p - 2 * this.q * other.p;
         return new Rational(newNumerator, plus.q);
     }
-
+    //This method returns a Rational obj that represents the multiply of this rational and param rational
     public Rational multiply(Rational other) { // (a/b) * (c/d) = (a*c)/(b*d)
         int newNumerator = this.p * other.p;
         int newDenominator = this.q * other.q;
         return new Rational(newNumerator, newDenominator);
     }
 
+    //This method returns a Rational obj that represents the divide value of this rational and param rational
     public Rational divide(Rational other) throws ArithmeticException {
-        if (other.p == 0) {
-            throw new ArithmeticException();
+        if (other.p == 0) { //(a/b)/(c/d)=(a/b)*(d/c) -> check if other denominator is zero
+            throw new ArithmeticException();//it's impossible to divide in zero
         }
         int newNumerator = this.p * other.q;
         int newDenominator = this.q * other.p;
         return new Rational(newNumerator, newDenominator);
     }
-
+    //reduce the rational number by Euclid algorithm
     public Rational reduce() {
         int gcdNum=gcd(this.p,this.q);
         int newNumerator = this.p / gcdNum;
@@ -71,6 +81,5 @@ public class Rational {
         if (b == 0) return a;
         return gcd(b, a % b);
     }
-
 
 }
